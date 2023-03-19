0c1d7a0da68b739813064ed055aa1da4cb9e7641
aquova <austinbricker@protonmail.com>
2023-03-19 08:23:32 -0500

    Remove optional LSP plugins

--- vim_install.sh
+++ vim_install.sh
@@ -33,12 +33,7 @@ PLUGINS=(
     'williamboman/mason-lspconfig.nvim'
     'hrsh7th/nvim-cmp'
     'hrsh7th/cmp-nvim-lsp'
-    'hrsh7th/cmp-buffer'
-    'hrsh7th/cmp-path'
-    'saadparwaiz1/cmp_luasnip'
-    'hrsh7th/cmp-nvim-lua'
     'L3MON4D3/LuaSnip'
-    'rafamadriz/friendly-snippets'
 )
 
 NVIMDIR=$HOME/.config/nvim;
