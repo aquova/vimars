e77865f865d388aff8ff7388454ae795d2fdb198
aquova <austinbricker@protonmail.com>
2023-06-16 11:52:02 -0500

    Add support for neovim-VSCode

--- init.lua
+++ init.lua
@@ -1,74 +1,77 @@
 -- Vimars init.lua
 -- Austin Bricker, 2017-2023
 
-require("dapui").setup()
-require("diffview").setup()
-require("gitsigns").setup()
 require("hop").setup()
-require("nvim-tree").setup()
 
-require("bufferline").setup{
-    auto_hide = true,
-    insert_at_end = true,
-}
+if not vim.g.vscode then
+    require("dapui").setup()
+    require("diffview").setup()
+    require("gitsigns").setup()
+    require("nvim-tree").setup()
 
-local dap = require("dap")
-dap.adapters.python = {
-    type = "executable",
-    command = "python",
-    args = { "-m", "debugpy.adapter" },
-    options = {
-        source_filetype = "python",
-    },
-}
-dap.configurations.python = {
-    {
-        type = "python",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}",
-        pythonPath = "/usr/bin/python"
-    },
-}
+    require("bufferline").setup{
+        auto_hide = true,
+        insert_at_end = true,
+    }
 
-require("lualine").setup{
-    options = {
-        icons_enabled = true,
-        section_separators = '',
-        component_separators = '',
-    },
-    sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch'},
-        lualine_c = {'diff'},
-        lualine_x = {'progress', 'location'},
-        lualine_y = {'fileformat', 'encoding'},
-        lualine_z = {'filetype', 'filename'},
-    },
-    tabline = {},
-    extensions = {},
-}
+    local dap = require("dap")
+    dap.adapters.python = {
+        type = "executable",
+        command = "python",
+        args = { "-m", "debugpy.adapter" },
+        options = {
+            source_filetype = "python",
+        },
+    }
+    dap.configurations.python = {
+        {
+            type = "python",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            pythonPath = "/usr/bin/python"
+        },
+    }
 
-local lsp = require("lsp-zero").preset({
-    name = 'minimal',
-    set_lsp_keymaps = true,
-    manage_nvim_cmp = true,
-    suggest_lsp_servers = false,
-})
-lsp.setup()
+    require("lualine").setup{
+        options = {
+            icons_enabled = true,
+            section_separators = '',
+            component_separators = '',
+        },
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch'},
+            lualine_c = {'diff'},
+            lualine_x = {'progress', 'location'},
+            lualine_y = {'fileformat', 'encoding'},
+            lualine_z = {'filetype', 'filename'},
+        },
+        tabline = {},
+        extensions = {},
+    }
 
-require("marks").setup{
-    builtin_marks = { ".", "<", ">", "^", "'" }
-}
+    local lsp = require("lsp-zero").preset({
+        name = 'minimal',
+        set_lsp_keymaps = true,
+        manage_nvim_cmp = true,
+        suggest_lsp_servers = false,
+    })
+    lsp.setup()
 
-require('onedark').setup {
-    style = 'darker'
-}
-require('onedark').load()
+    require("marks").setup{
+        builtin_marks = { ".", "<", ">", "^", "'" }
+    }
 
-require("toggleterm").setup{
-    direction = 'float'
-}
+    require('onedark').setup {
+        style = 'darker'
+    }
+    require('onedark').load()
+
+    require("toggleterm").setup{
+        direction = 'float'
+    }
+end
 
 local set = vim.opt
 set.background = "dark"
@@ -134,16 +137,22 @@ vnomap("B", "^")
 nnomap("E", "$")
 vnomap("E", "$")
 
-nnomap("J", ":bp<CR>")
-nnomap("K", ":bn<CR>")
-nnomap("gJ", "J<CR>")
-nnomap("gK", "K<CR>")
-inomap("jk", "<esc>")
+if vim.g.vscode then
+    -- gt/gT also performs this by default
+    nnomap("J", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+    nnomap("K", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
+else
+    nnomap("J", ":bp<CR>")
+    nnomap("K", ":bn<CR>")
+    nnomap("gJ", "J<CR>")
+    nnomap("gK", "K<CR>")
+    nnomap("L", "L<bar>zz<CR>")
+    vnomap("L", "L<bar>zz<CR>")
+    nnomap("H", "H<bar>zz<CR>")
+    vnomap("H", "H<bar>zz<CR>")
+end
 
-nnomap("L", "L<bar>zz<CR>")
-vnomap("L", "L<bar>zz<CR>")
-nnomap("H", "H<bar>zz<CR>")
-vnomap("H", "H<bar>zz<CR>")
+inomap("jk", "<esc>")
 
 nnomap("<leader><space>", ":noh<CR>")
 
@@ -156,24 +165,39 @@ vnomap("Y", "y$")
 nnomap("[<space>", "O<esc>j")
 nnomap("]<space>", "o<esc>k")
 
-nnomap("<leader>b", ":Gitsigns toggle_current_line_blame<CR>")
-nnomap("<leader>g", ":Gitsigns reset_hunk<CR>")
+if vim.g.vscode then
+    nnomap("J", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+    nnomap("K", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
+    nnomap("gJ", "<Cmd>call VSCodeNotify('editor.action.joinLines')<CR>")
 
-nnomap("]c", ":Gitsigns next_hunk<CR>")
-nnomap("[c", ":Gitsigns prev_hunk<CR>")
+    nnomap("[c", "<Cmd>call VSCodeNotify('workbench.action.editor.previousChange')<CR>")
+    nnomap("]c", "<Cmd>call VSCodeNotify('workbench.action.editor.nextChange')<CR>")
 
-nnomap("<C-t>", ":ToggleTerm<CR>")
-tnomap("<C-t>", "<C-\\><C-N>:ToggleTerm<CR>")
+    nnomap("[p", "<Cmd>call VSCodeNotify('editor.debug.action.goToPreviousBreakpoint')<CR>")
+    nnomap("]p", "<Cmd>call VSCodeNotify('editor.debug.action.goToNextBreakpoint')<CR>")
 
-nnomap("<leader>t", ":NvimTreeToggle<CR>")
+    nnomap("gp", "<Cmd>call VSCodeNotify('editor.debug.action.toggleBreakpoint')<CR>")
+    nnomap("<leader>g", "<Cmd>call VSCodeNotify('git.revertSelectedRanges')<CR>")
+else
+    nnomap("<leader>b", ":Gitsigns toggle_current_line_blame<CR>")
+    nnomap("<leader>g", ":Gitsigns reset_hunk<CR>")
 
-nnomap("<C-f>",     ":Telescope live_grep<CR>")
-nnomap("<C-p>",     ":Telescope find_files<CR>")
-nnomap("<leader>d", ":lua DiffviewToggle()<CR>")
-nnomap("<leader>z", ":Telescope spell_suggest<CR>")
-nnomap("<leader>m", ":Telescope man_pages<CR>")
-nnomap("<leader>x", ":lua require('dapui').toggle()<CR>")
-nnomap("<leader>p", ":lua require('dap').toggle_breakpoint()<CR>")
+    nnomap("]c", ":Gitsigns next_hunk<CR>")
+    nnomap("[c", ":Gitsigns prev_hunk<CR>")
+
+    nnomap("<C-t>", ":ToggleTerm<CR>")
+    tnomap("<C-t>", "<C-\\><C-N>:ToggleTerm<CR>")
+
+    nnomap("<leader>t", ":NvimTreeToggle<CR>")
+
+    nnomap("<C-f>",     ":Telescope live_grep<CR>")
+    nnomap("<C-p>",     ":Telescope find_files<CR>")
+    nnomap("<leader>d", ":lua DiffviewToggle()<CR>")
+    nnomap("<leader>z", ":Telescope spell_suggest<CR>")
+    nnomap("<leader>m", ":Telescope man_pages<CR>")
+    nnomap("<leader>x", ":lua require('dapui').toggle()<CR>")
+    nnomap("<leader>p", ":lua require('dap').toggle_breakpoint()<CR>")
+end
 
 nnomap("<leader><leader>w", ":HopWordAC<CR>")
 nnomap("<leader><leader>b", ":HopWordBC<CR>")
@@ -183,13 +207,7 @@ nnomap("<leader><leader>j", ":HopLineStartAC<CR>")
 nnomap("<leader><leader>k", ":HopLineStartBC<CR>")
 nnomap("<leader><leader>/", ":HopPattern<CR>")
 
-vim.g.closetag_filetypes = 'html, xhtml, phtml, php'
-
--- Strip trailing whitespace
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*" },
-    command = [[%s/\s\+$//e]],
-})
+vim.g.closetag_filetypes = 'html, php'
 
 -- TODO: Figure out if this can be done with just Lua
 vim.cmd([[
@@ -199,15 +217,23 @@ au TextYankPost * silent! lua require'vim.highlight'.on_yank({hlgroup="IncSearch
 augroup END
 ]])
 
--- Custom function to add toggle functionality to diffview.nvim
-local last_tabpage = vim.api.nvim_get_current_tabpage()
-function DiffviewToggle()
-    local lib = require("diffview.lib")
-    local view = lib.get_current_view()
-    if view then
-        vim.cmd(":DiffviewClose")
-    else
-        vim.cmd(":DiffviewOpen")
+if not vim.g.vscode then
+    -- Strip trailing whitespace
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = { "*" },
+        command = [[%s/\s\+$//e]],
+    })
+
+    -- Custom function to add toggle functionality to diffview.nvim
+    local last_tabpage = vim.api.nvim_get_current_tabpage()
+    function DiffviewToggle()
+        local lib = require("diffview.lib")
+        local view = lib.get_current_view()
+        if view then
+            vim.cmd(":DiffviewClose")
+        else
+            vim.cmd(":DiffviewOpen")
+        end
     end
 end
 
